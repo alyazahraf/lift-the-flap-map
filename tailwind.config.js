148492bbeb6f3deae88d63805985153643621ac5
alyazahraf <82332833+alyazahraf@users.noreply.github.com>
2023-11-29 20:42:00 +0700

    update instruction

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray': '#00000012',
+        'button-gray': "#CFCFCF"
+
+      }
+    },
   },
   plugins: [],
 }
